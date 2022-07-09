948ac96afa4a8d38bdaf6592abfb4ac1993a004c
nitrofimenko <n.i.trofimenko@hotmail.com>
2022-07-09 20:59:50 +0300

    feat: Add L01-02

---
+++ src/L01-02/Program.cs
@@ -0,0 +1,9 @@
+﻿using L01_02;
+
+Console.Write("Input size of rectangle side 1: ");
+double rectangleSide1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input size of rectangle side 2: ");
+double rectangleSide2 = Convert.ToDouble(Console.ReadLine());
+Rectangle userRectangle = new Rectangle(rectangleSide1,rectangleSide2);
+Console.WriteLine($"Rectangle perimeter is {userRectangle.Perimeter}.");
+Console.WriteLine($"Rectangle area is {userRectangle.Area}.");
