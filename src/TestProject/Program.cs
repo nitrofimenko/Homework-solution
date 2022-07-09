3b59446291e316793d08387a8052c8664b33f804
nitrofimenko <n.i.trofimenko@hotmail.com>
2022-07-09 13:58:11 +0300

    fix: fix review comment

--- src/TestProject/Program.cs
+++ src/TestProject/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+string outMessage = "Hello, World!"; 
+Console.WriteLine(outMessage);
 Console.WriteLine("new text");
