07017ccf33ea6b3243068a387815694307421e2e
nitrofimenko <n.i.trofimenko@hotmail.com>
2022-07-09 18:40:52 +0300

    feat: Make L01 project

---
+++ src/L01/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L01
+{
+    internal class Address
+    {
+        public string Index { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string House { get; set; }
+        public string Apartment { get; set; }
+    }
+}
---
+++ src/L01/Program.cs
@@ -0,0 +1,15 @@
+﻿using L01;
+
+Address instance = new Address();
+instance.Index = "08720";
+instance.Country = "Krakozia";
+instance.City = "Default";
+instance.Street = "Main Street";
+instance.House = "221B";
+instance.Apartment = "Penthouse";
+Console.WriteLine($"Index\t\t{instance.Index}\n" +
+    $"Country\t\t{instance.Country}\n" +
+    $"City\t\t{instance.City}\n" +
+    $"Street\t\t{instance.Street}\n" +
+    $"House\t\t{instance.House}\n" +
+    $"Apartment\t{instance.Apartment}\n");
