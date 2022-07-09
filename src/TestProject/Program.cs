8aa7ab35f2f8bf937b1f452281eba2c07aa58a25
nitrofimenko <n.i.trofimenko@hotmail.com>
2022-07-09 18:06:43 +0300

    Delete TestProj

--- src/TestProject/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using TestProject.Class;
-
-string outMessage = "Hello, World!";
-TestClass.ShowMessage(outMessage);
-Console.WriteLine("new text");
