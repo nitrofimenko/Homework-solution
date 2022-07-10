346a9e69a361b466a8c0354cd443f108b1d690a7
nitrofimenko <n.i.trofimenko@hotmail.com>
2022-07-10 18:09:28 +0300

    feat: Add L01-03

---
+++ src/L01-03/Book/Parts/Title.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L01_03.Book.Parts
+{
+    internal class Title
+    {
+        public string title;
+        public void Show()
+        {
+            Console.ForegroundColor=ConsoleColor.Red;
+            Console.WriteLine(title);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
