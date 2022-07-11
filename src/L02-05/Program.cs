c3df7b05977910068dd188085aafe0c686cb2cdc
nitrofimenko <n.i.trofimenko@hotmail.com>
2022-07-11 23:22:26 +0300

    feat: Add L02-05

---
+++ src/L02-05/Program.cs
@@ -0,0 +1,52 @@
+﻿using L02_05;
+
+const int CAPACITY = 100;
+User[] users = new User[CAPACITY];
+int usersNumber = 0;
+do
+{
+    Console.WriteLine(usersNumber != CAPACITY ? "1 - create new user" : "1 is unavailable (Can't create new user. Maximum capacity reached)");
+    if (usersNumber > 0)
+    {
+        Console.WriteLine($"2 - show all {usersNumber} users");
+        if (usersNumber > 1)
+        {
+            Console.WriteLine("3 - show #{your number} user");
+        }
+    }
+    Console.Write("e - exit\nChoose one of this: ");
+
+    var choose = Console.ReadLine();
+    Console.WriteLine();
+
+    if (choose == "1" && usersNumber != CAPACITY)
+    {
+        users[usersNumber] = new User();
+        Console.WriteLine($"User #{usersNumber} was succesfully created\n");
+        usersNumber++;
+    }
+    else if (choose == "2" && usersNumber > 0)
+    {
+        for (int i = 0; i < usersNumber; i++)
+        {
+            Console.WriteLine($"About user #{i} information");
+            users[i].Show();
+            Console.WriteLine();
+        }
+    }
+    else if (choose == "3" && usersNumber > 1)
+    {
+        int userNumber = 0;
+        do
+        {
+            Console.Write($"\nInput user number from 0 to {usersNumber - 1}: ");
+        } while (!int.TryParse(Console.ReadLine(), out userNumber) || userNumber < 0 || userNumber >= usersNumber);
+        Console.WriteLine($"About user #{userNumber} information");
+        users[userNumber].Show();
+        Console.WriteLine();
+    }
+    else if (choose == "e")
+    {
+        break;
+    }
+} while (true);
---
+++ src/L02-05/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L02_05
+{
+    internal class User
+    {
+        string login = "";
+        string firstName = "";
+        string secondName = "";
+        int age;
+        readonly DateTime formFillingDate;
+        public User()
+        {
+            Console.WriteLine("Creation new user");
+
+            Console.Write("Input new user login: ");
+            login = Console.ReadLine();
+
+            Console.Write("Input new user first name: ");
+            firstName = Console.ReadLine();
+
+            Console.Write("Input new user second name: ");
+            secondName = Console.ReadLine();
+
+            do
+            {
+                Console.Write("Input new user age (integer number): ");
+            } while (!int.TryParse(Console.ReadLine(), out age) || age <= 0); ;
+
+            formFillingDate = DateTime.Now;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Login:             {login}");
+            Console.WriteLine($"First name:        {firstName}");
+            Console.WriteLine($"Second name:       {secondName}");
+            Console.WriteLine($"Age on {formFillingDate:dd.MM.yyyy}: {age}");
+        }
+    }
+}
