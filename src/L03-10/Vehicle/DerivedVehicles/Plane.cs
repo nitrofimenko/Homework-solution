36634d97711ac50d553daa64aa24426dec2ab2f1
nitrofimenko <n.i.trofimenko@hotmail.com>
2022-08-13 20:18:01 +0300

    feat: add L03-10

---
+++ src/L03-10/Vehicle/DerivedVehicles/Plane.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L03_10.Vehicle.DerivedVehicles
+{
+    internal class Plane:Vehicle
+    {
+        private double altitude;
+        private int numberOfPassengers;
+        public Plane(double x, double y, double price, double speed, int yearOfManufacture, double altitude, int numberOfPassengers)
+        {
+            this.x = x;
+            this.y = y;
+            this.price = price;
+            this.speed = speed;
+            this.yearOfManufacture = yearOfManufacture;
+            this.altitude = altitude;
+            this.numberOfPassengers = numberOfPassengers;
+        }
+        public override void Show()
+        {
+            base.Show();
+            Console.Write($", altitude = {altitude}, number of passengers = {numberOfPassengers}");
+        }
+    }
+}
---
+++ src/L03-10/Vehicle/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L03_10.Vehicle
+{
+    internal class Vehicle
+    {
+        protected double x = 0;
+        protected double y = 0;
+        protected double price = 0;
+        protected double speed = 0;
+        protected double yearOfManufacture = 0;
+        public virtual void Show()
+        {
+            Console.Write($"x = {x}, y = {y}, price = {price}, speed = {speed}, year of manufacture: {yearOfManufacture}");
+        }
+    }
+}
