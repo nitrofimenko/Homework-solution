7120f29b098bb32537a512198ade36e0d23dc832
nitrofimenko <n.i.trofimenko@hotmail.com>
2022-08-15 19:43:14 +0300

    fix: rename Class to classRoom

--- src/L03-09/Program.cs
+++ src/L03-09/Program.cs
@@ -6,15 +6,15 @@ Console.WriteLine("Thnx MS for help");
 string choice;
 do
 {
-    ClassRoom Class = new ClassRoom(new ExcelentPupil(), new Pupil());
+    ClassRoom classRoom = new ClassRoom(new ExcelentPupil(), new Pupil());
     Console.WriteLine("Constructor 1 (two base pupils)");
-    Class.Show();
+    classRoom.Show();
     Console.Write("Key to show next ");
     Console.ReadKey();
     Console.WriteLine();
-    Class = new ClassRoom(new ExcelentPupil(), new GoodPupil(), new BadPupil());
+    classRoom = new ClassRoom(new ExcelentPupil(), new GoodPupil(), new BadPupil());
     Console.WriteLine("Constructor 2 (three base pupils)");
-    Class.Show();
+    classRoom.Show();
     Console.Write("\"x\" to exit, another to continue: ");
     choice = Console.ReadLine();
 } while (choice != "x");
