86e8e0328d5a6d9bc591d99b5e4307122b319ee1
nitrofimenko <n.i.trofimenko@hotmail.com>
2022-08-26 22:58:51 +0300

    fix: change class Dimension to static class Dim Program.cs changes accordingly

---
+++ src/L05-15/Dim.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L05_15
+{
+    internal static class Dim
+    {
+        public static int[] Init(int N)
+        {
+            N = N > 0 ? N : 0;
+            int[] array = new int[N];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next();
+            }
+            return array;
+        }
+        public static int GetMax(int[] array)
+        {
+            if (array.Length == 0)
+            {
+                return 0;
+            }
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+            return max;
+        }
+        public static int GetMin(int[] array)
+        {
+            if (array.Length == 0)
+            {
+                return 0;
+            }
+            int min = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+            return min;
+        }
+        public static long GetSum(int[] array)
+        {
+            if (array.Length == 0)
+            {
+                return 0;
+            }
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+        public static int[] GetOddElements(int[] array)
+        {
+            int[] temp = new int[array.Length];
+            int j = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    temp[j++] = array[i];
+                }
+            }
+            int[] oddElements = new int[j];
+            Array.Copy(temp, oddElements, j);
+            return oddElements;
+        }
+    }
+}
--- src/L05-15/Program.cs
+++ src/L05-15/Program.cs
@@ -2,15 +2,15 @@
 using L05_15;
 
 const int N = 20;
-Dimension dimension = new Dimension(N);
+int[] array = Dim.Init(N);
 
 NumberFormatInfo nfi = new CultureInfo("").NumberFormat;
 nfi.NumberGroupSeparator = "'";
-Console.WriteLine($"Maximum array element = {dimension.Max.ToString("N0", nfi)}");
-Console.WriteLine($"Minimum array element = {dimension.Min.ToString("N0", nfi)}");
-Console.WriteLine($"Total sum of array elements = {dimension.Sum.ToString("N0", nfi)}");
+Console.WriteLine($"Maximum array element = {Dim.GetMax(array).ToString("N0", nfi)}");
+Console.WriteLine($"Minimum array element = {Dim.GetMin(array).ToString("N0", nfi)}");
+Console.WriteLine($"Total sum of array elements = {Dim.GetSum(array).ToString("N0", nfi)}");
 Console.WriteLine("All odd elements in array:");
-int[] dimensionOddElements = dimension.OddElements;
+int[] dimensionOddElements = Dim.GetOddElements(array);
 for (int i = 0; i < dimensionOddElements.Length; i++)
 {
     Console.WriteLine(dimensionOddElements[i].ToString("N0", nfi));
