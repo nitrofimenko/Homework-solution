c3e8cd82eccfc4bbfb8482b41c7ddacc614d52a5
Николай Ильич Трофименко <n.i.trofimenko@hotmail.com>
2022-08-30 14:57:09 +0300

    feat: Add L05-18

---
+++ src/L05-18/Dictionary.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L05_18
+{
+    internal class Dictionary
+    {
+        private static int LANGS_NUM = 3;
+        private static int WORDS_NUM = 6;
+        private string[,] words = new string[LANGS_NUM, WORDS_NUM];
+        public Dictionary()
+        {
+            words[0, 0] = "RU"; words[1, 0] = "EN"; words[2, 0] = "UA";
+            words[0, 1] = "книга"; words[1, 1] = "book"; words[2, 1] = "книга";
+            words[0, 2] = "ручка"; words[1, 2] = "pen"; words[2, 2] = "ручка";
+            words[0, 3] = "солнце"; words[1, 3] = "sun"; words[2, 3] = "сонце";
+            words[0, 4] = "яблоко"; words[1, 4] = "apple"; words[2, 4] = "яблуко";
+            words[0, 5] = "стол"; words[1, 5] = "table"; words[2, 5] = "стіл";
+        }
+        public int GetLanguagesNumber() => words.GetLength(0);
+        public string GetLanguage(int languageNumber) => words[0, languageNumber];
+        public string this[string word, string langFrom, string langTo]
+        {
+            get
+            {
+                int indexFrom = -1;
+                int indexTo = -1;
+                for (int i = 0; i < words.GetLength(0); i++)
+                {
+                    if (words[i, 0] == langFrom.ToUpper())
+                    {
+                        indexFrom = i;
+                    }
+                    if (words[i, 0] == langTo.ToUpper())
+                    {
+                        indexTo = i;
+                    }
+                }
+                if (indexFrom < 0 || indexTo < 0)
+                {
+                    string report = "";
+                    if (indexFrom < 0)
+                    {
+                        report += string.Format($"Language {langFrom} invalid.\n");
+                    }
+                    if (indexTo < 0)
+                    {
+                        report += string.Format($"Language {langTo} invalid.\n");
+                    }
+                    report += "Valid languages collection:";
+                    for (int i = 0; i < words.GetLength(0); i++)
+                    {
+                        report += " " + words[i, 0].ToString();
+                    }
+                    return report;
+                }
+                for (int i = 0; i < words.GetLength(1); i++)
+                {
+                    if (words[indexFrom, i] == word)
+                    {
+                        return words[indexTo, i];
+                    }
+                }
+                return string.Format($"Not found translate to \"{word}\" from {langFrom} to {langTo}");
+            }
+        }
+    }
+}
---
+++ src/L05-18/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using L05_18;
+
+void ShowTranslate(Dictionary dictionary, string word, string langFrom, string langTo)
+{
+    Console.WriteLine($"Trying translate \"{word}\" from {langFrom} to {langTo}... " +
+        $"{dictionary[word, langFrom, langTo]}");
+}
+Dictionary dictionary = new Dictionary();
+string ru = "ru";
+string en = "en";
+string ua = "ua";
+ShowTranslate(dictionary, "книга", ru, en);
+ShowTranslate(dictionary, "дом", ru, en);
+ShowTranslate(dictionary, "ручка", ru, en);
+ShowTranslate(dictionary, "стол", ru, en);
+ShowTranslate(dictionary, "карандаш", ru, en);
+ShowTranslate(dictionary, "книга", ru, en);
+ShowTranslate(dictionary, "яблоко", ru, en);
+ShowTranslate(dictionary, "солнце", ru, en);
