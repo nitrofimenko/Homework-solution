a090b75bc8a7091dc51dabafff977e879b93b44c
Николай Ильич Трофименко <n.i.trofimenko@hotmail.com>
2022-08-31 09:53:43 +0300

    feat: Add L06-19

---
+++ src/L06-19/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L06_19
+{
+    internal class Book
+    {
+        public void FindNext(string str)
+        {
+            Console.WriteLine("Find string: " + str);
+        }
+    }
+}
