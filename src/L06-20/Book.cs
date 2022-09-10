ff0cda444f7e960c09b0766cd3ef44e4c6feab25
nitrofimenko <n.i.trofimenko@hotmail.com>
2022-09-10 21:23:22 +0300

    feat: Add L06-20

---
+++ src/L06-20/Book.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L06_20
+{
+    internal class Book
+    {
+        public void FindNext(string str)
+        {
+            Console.WriteLine("Поиск строки : " + str);
+        }
+        public int AddNote(string note) => Notes.AddNote(note);
+        public void ChangeNote(int index, string note) => Notes.ChangeNote(index, note);
+        public string GetNote(int index) => Notes.GetNote(index);
+        public void ShowNote(int index) => Notes.ShowNote(index);
+        private static class Notes
+        {
+            private static string[] notes = new string[100];
+            private static int noteMaxNumber = -1;
+            public static int AddNote(string note)
+            {
+                notes[++noteMaxNumber] = note;
+                return noteMaxNumber;
+            }
+            public static void ChangeNote(int index, string note)
+            {
+                if (index >= 0 && index <= noteMaxNumber)
+                {
+                    notes[index] = note;
+                }
+            }
+            public static string GetNote(int index) => (noteMaxNumber < 0 || index < 0 || index > noteMaxNumber) ? "" : notes[index];
+            public static void ShowNote(int index)
+            {
+                if (noteMaxNumber < 0)
+                {
+                    Console.WriteLine("No notes");
+                }
+                else
+                {
+                    if (index < 0 || index > noteMaxNumber)
+                    {
+                        Console.WriteLine($"Not have note #{index}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Note #{index}: {notes[index]}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/L06-20/Program.cs
@@ -0,0 +1,20 @@
+﻿using L06_20;
+
+Book book = new Book();
+
+Console.Write("Book notes empty. Note #0: ");
+book.ShowNote(0);
+Console.WriteLine($"GetNote() test. GetNote(0): {book.GetNote(0)}");
+
+int note1Number = book.AddNote("test0");
+Console.WriteLine($"Note #{note1Number} added.");
+book.ShowNote(note1Number);
+
+book.ChangeNote(note1Number, "test1");
+Console.WriteLine($"Note #{note1Number} changed.");
+Console.WriteLine($"Note #{note1Number}: {book.GetNote(note1Number)}");
+
+Console.Write("Note #1: ");
+book.ShowNote(1);
+Console.Write("Note #-1: ");
+book.ShowNote(-1);
