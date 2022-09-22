248cb16e430ce63085ed7d9f8c6efd42bbbd68ba
Николай Ильич Трофименко <n.i.trofimenko@hotmail.com>
2022-09-22 12:32:50 +0300

    feat: Add L07-23

---
+++ src/L07-23/L07-23/Notebook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L07_23
+{
+    struct Notebook
+    {
+        private string model;
+        private string manufacturer;
+        private double price;
+        public Notebook(string model, string manufacturer, double price)
+        {
+            this.model = model;
+            this.manufacturer = manufacturer;
+            this.price = price;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Model: {model}");
+            Console.WriteLine($"Manufacturer: {manufacturer}");
+            Console.WriteLine($"Price: {price}");
+        }
+    }
+}
---
+++ src/L07-23/L07-23/Program.cs
@@ -0,0 +1,4 @@
+﻿using L07_23;
+
+Notebook notebook=new Notebook("ThinkPad X60 Tablet","IBM",4295.00);
+notebook.Show();
