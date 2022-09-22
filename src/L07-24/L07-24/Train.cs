e68664efff71b60c7ad5b3c96df8582ab8dfed40
Николай Ильич Трофименко <n.i.trofimenko@hotmail.com>
2022-09-22 13:06:55 +0300

    fix: delete L07-23, L07-24

---
+++ src/L07-24/L07-24/Train.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L07_24
+{
+    internal class Train
+    {
+    }
+}
