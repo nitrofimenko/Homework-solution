e79c017c588838ed6ea7667eb462743bf02ad726
nitrofimenko <n.i.trofimenko@hotmail.com>
2022-10-02 19:35:51 +0300

    feat: Add L07-25

---
+++ src/L07-25/MyStruct.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L07_25
+{
+    internal class MyStruct
+    {
+        public string change;
+    }
+}
---
+++ src/L07-25/Program.cs
@@ -0,0 +1,22 @@
+﻿using L07_25;
+
+static void ClassTaker(MyClass myClass)
+{
+    myClass.change = "changed";
+}
+static void StructTaker(MyStruct myStruct)
+{
+    myStruct.change = "changed";
+}
+
+MyStruct myStruct = new MyStruct();
+MyClass myClass = new MyClass();
+
+myStruct.change = "non changed";
+myClass.change = "non changed";
+
+StructTaker(myStruct);
+ClassTaker(myClass);
+
+Console.WriteLine($"Struct field \"change\" value: {myStruct.change}");
+Console.WriteLine($"Class field \"change\" value: {myClass.change}");
