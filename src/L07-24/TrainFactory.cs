242fdda8df53dea062d0da0862af2dfedf825b4b
nitrofimenko <n.i.trofimenko@hotmail.com>
2022-10-02 18:48:52 +0300

    fix: change search algorythm

--- src/L07-24/TrainFactory.cs
+++ src/L07-24/TrainFactory.cs
@@ -8,20 +8,16 @@ namespace L07_24
 {
     static internal class TrainFactory
     {
-        private static string destination;
-        private static int trainNumber;
-        private static DateTime departureTime;
-        public static Train CreateTrain(int trainNumber)
+        public static Train CreateTrain(int recordNumber)
         {
-            Console.WriteLine($"Train {trainNumber}");
+            Console.WriteLine($"Train {recordNumber}");
             Console.Write("Destination: ");
-            destination = Console.ReadLine();
+            string destination = Console.ReadLine();
             Console.Write("Train number: ");
-            trainNumber = Convert.ToInt32(Console.ReadLine());
+            int trainNumber = Convert.ToInt32(Console.ReadLine());
             Console.Write("Departure time: ");
-            departureTime = Convert.ToDateTime(Console.ReadLine());
+            DateTime departureTime = Convert.ToDateTime(Console.ReadLine());
             Console.WriteLine(new String('-', 20));
-            int position;
             return new Train(destination, trainNumber, departureTime);
         }
     }
--- src/L07-24/TrainList.cs
+++ src/L07-24/TrainList.cs
@@ -36,16 +36,10 @@ namespace L07_24
             trains[trainsNumber] = TrainFactory.CreateTrain(trainsNumber);
             trainsNumber++;
         }
-        class TrainNumberComparer : IComparer<Train>
-        {
-            public int Compare(Train train1, Train train2)
-            {
-                return train1.TrainNumber.CompareTo(train2.TrainNumber);
-            }
-        }
         public void SortByTrainNumber()
         {
-            Array.Sort(trains, 0, trainsNumber - 1, new TrainNumberComparer());
+            IEnumerable<Train> query = trains.OrderBy(train => train.TrainNumber);
+            trains = query.ToArray();
         }
         public void SearchAndShow(int trainNumber)
         {
