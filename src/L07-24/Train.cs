9972041aac6f23cf2e55e5ae2f8cdc2346591e74
Николай Ильич Трофименко <n.i.trofimenko@hotmail.com>
2022-10-03 08:20:23 +0300

    fix: pull from github

---
+++ src/L07-24/Train.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L07_24
+{
+    struct Train
+    {
+        private string destination;
+        private string trainNumber;
+        private DateTime departureTime;
+        public string TrainNumber { get { return trainNumber; } }
+        public Train(string destination,string trainNumber,DateTime departureTime)
+        {
+            this.destination = destination;
+            this.trainNumber = trainNumber;
+            this.departureTime = departureTime;
+        }
+        public void Show()
+        {
+            Console.WriteLine($"Destination: {destination}\nTrain number: {trainNumber}\nDeparture time: {departureTime:H:mm}");
+        }
+    }
+}
