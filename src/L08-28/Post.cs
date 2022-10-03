639004e6ae55ec055717eace9dee7f0373634775
nitrofimenko <n.i.trofimenko@hotmail.com>
2022-10-03 22:58:48 +0300

    feat: Add L08-28

---
+++ src/L08-28/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L08_28
+{
+    internal enum Post
+    {
+        Slave = 400,
+        Candidate = 360,
+        Junior = 300,
+        Pre_intermediate = 225,
+        Middle = 170,
+        Senior = 120,
+        TeamLead = 80,
+        Architect = 40,
+        Scram_master = 20,
+        CEO = 0
+    }
+}
---
+++ src/L08-28/Program.cs
@@ -0,0 +1,15 @@
+﻿using L08_28;
+
+int[] post = (int[])Enum.GetValues(typeof(Post));
+
+Random random = new Random();
+
+string choice = "";
+Console.WriteLine("0 for exit, any other - continue. ");
+while (choice != "0")
+{
+    Post worker = (Post)post[random.Next(post.Length)];
+    int hours = random.Next(post.Min(),post.Max());
+    Console.Write($"{worker,-20}\t{hours,3} hrs (min - {(int)worker})\t{Accauntant.AskForBonus(worker, hours)}  ");
+    choice = Console.ReadLine();
+}
