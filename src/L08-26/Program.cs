b8511d5af83637930695d152b1da5f100104469a
nitrofimenko <n.i.trofimenko@hotmail.com>
2022-10-03 20:21:18 +0300

    feat: Add L08-26

---
+++ src/L08-26/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Date of Birth: ");
+DateTime dateOfBirth = Convert.ToDateTime(Console.ReadLine()).Date;
+Console.WriteLine(dateOfBirth.AddYears(DateTime.Now.Date.Year - dateOfBirth.Year) > DateTime.Now.Date ?
+    (dateOfBirth.AddYears(DateTime.Now.Date.Year - dateOfBirth.Year) - DateTime.Now.Date).Days :
+    (dateOfBirth.AddYears(DateTime.Now.Date.Year + 1 - dateOfBirth.Year) - DateTime.Now.Date).Days);
