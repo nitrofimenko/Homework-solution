6b57208385e80823e721f211e8f287ac9cd08c99
nitrofimenko <n.i.trofimenko@hotmail.com>
2022-10-13 18:05:04 +0300

    feat: Add L09-29

---
+++ src/L09-29/ArithmeticMean.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L09_29
+{
+    public delegate double ArithmeticMean(int first, int second, int third);
+}
